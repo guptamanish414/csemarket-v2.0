17a6e5137c2ad9c609fc933131d7644647866290
manishguptav2 <manishgupta@v2solutions.com>
2018-07-24 08:31:31 +0530

    static file read in express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
+app.use(express.static('frontend/public'));
 
-app.get('/', (req, res) => res.sendFile('./frontend/index.html', 
-{root:__dirname}))
+app.get('/', (req, res) => res.sendFile('./frontend/index.html', {root:__dirname}));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
